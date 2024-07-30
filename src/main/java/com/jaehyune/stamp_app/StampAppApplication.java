04f166671d3a78611720cc9fc9c3d173f1c89c58
Jae Hyune Yea <jaehyune.yea@gmail.com>
2024-07-30 01:03:48 -0700

    Postgres integration in application.properties

---
+++ src/main/java/com/jaehyune/stamp_app/StampAppApplication.java
@@ -0,0 +1,13 @@
+package com.jaehyune.stamp_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StampAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StampAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jaehyune/stamp_app/StampAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaehyune.stamp_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StampAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
